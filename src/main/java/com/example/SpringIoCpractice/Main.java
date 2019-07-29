45c76f6db29a77bc7110bbde46409c901f6c2c8a
wanghc00 <306093226@qq.com>
2019-07-29 11:30:39 +0800

    作业提交

--- src/main/java/com/example/SpringIoCpractice/Main.java
+++ src/main/java/com/example/SpringIoCpractice/Main.java
@@ -1,1 +1,19 @@
-//TODO
+package com.example.SpringIoCpractice;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Main implements ApplicationRunner{
+    
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        // TODO Auto-generated method stub
+        ApplicationContext context = new ClassPathXmlApplicationContext("hello.xml");
+        HelloWorld hw=(HelloWorld)context.getBean("helloWorld");
+        hw.getHello();
+    }
+}
